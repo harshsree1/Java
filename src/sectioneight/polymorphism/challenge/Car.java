aaaffa73ca8143b7a18c9948f21d960cdb4c98e2
sreeharsha <harshasree1@gmail.com>
2025-01-23 21:32:51 -0800

    Adding recent code

--- src/sectioneight/polymorphism/challenge/Car.java
+++ src/sectioneight/polymorphism/challenge/Car.java
@@ -42,3 +42,56 @@ class GasPoweredCar extends Car {
         System.out.printf("Gas -> Usage exceeds the average: %.3f %n", avgKmPerLiter);
     }
 }
+
+class ElectricCar extends Car {
+    private double avgKmPerCharge;
+    private int batterySize =6;
+
+    public ElectricCar(String description) {
+        super(description);
+    }
+
+    public ElectricCar(String description, double avgKmPerCharge, int batterySize) {
+        super(description);
+        this.avgKmPerCharge = avgKmPerCharge;
+        this.batterySize = batterySize;
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.printf("BEV -> switch %d kwh battery on, Ready!%n", batterySize);
+    }
+
+    @Override
+    protected void runEngine() {
+        System.out.printf("BEV -> Usage under the average: %.3f %n", avgKmPerCharge);
+    }
+}
+
+class HybridCar extends Car {
+    private double avgKmPerLiter;
+    private int cylinders =6;
+    private int batterySize;
+
+    public HybridCar(String description) {
+        super(description);
+    }
+
+    public HybridCar(String description, double avgKmPerLiter, int cylinders, int batterySize) {
+        super(description);
+        this.avgKmPerLiter = avgKmPerLiter;
+        this.cylinders = cylinders;
+        this.batterySize = batterySize;
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.printf("Hybrid -> %d cylinders are fired up,%n", cylinders);
+        System.out.printf("Hybrid -> switch %d kwh battery on, Ready!%n", batterySize);
+    }
+
+    @Override
+    protected void runEngine() {
+        System.out.printf("Hybrid -> Usage below  average: %.3f %n", avgKmPerLiter);
+    }
+}
--- src/sectioneight/polymorphism/challenge/Main.java
+++ src/sectioneight/polymorphism/challenge/Main.java
@@ -6,9 +6,13 @@ public class Main {
     runRace(car);
     Car ferrari = new GasPoweredCar("2022 Blue Ferrari 296 GTS", 15.4,6);
         runRace(ferrari);
+    Car tesla =new ElectricCar("2023 red Model y", 568,75);
+        runRace(tesla);
+    Car ferrari01 =new HybridCar("Ferrari new mode",15, 8, 8);
+    runRace(ferrari01);
     }
     public static void runRace(Car car){
-        car.startEngine();;
+        car.startEngine();
         car.drive();
     }
 }
