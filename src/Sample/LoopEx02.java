9a4ddf5df27d33aa5442509e4ecdfb9703d7f580
sreeharsha <harshasree1@gmail.com>
2025-06-02 22:14:04 -0700

    Adding recent code

---
+++ src/Sample/LoopEx02.java
@@ -0,0 +1,18 @@
+package Sample;
+
+import java.util.Scanner;
+
+public class LoopEx02 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int sum =0;
+        while(true) {
+            System.out.println(" enter a number: ");
+            sum += s.nextInt();
+            if (sum < 100)
+                break;
+
+        }
+        System.out.println("Done");
+    }
+}
